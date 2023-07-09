275ecbdd19e2ca7eac2d2fc57177aa4b73e1cab3
kushuaiming <xm2534911078@outlook.com>
2023-07-09 20:33:55 +0800

    feat: echo bash pid

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-## Command Comment
+## 整体设计思路
+方块自动下降 > 从键盘读取相关指令 > 根据指令作出相应的动作 > 自动消除并增加计分
+## 指令解释
 - read in silent mode and read only one characters of input.
 ```read -s -n 1 command```
 - echo without a newline
--- my_tetris.sh
+++ my_tetris.sh
@@ -11,10 +11,21 @@ DELAY=1          # initial delay between piece movements
 DELAY_FACTOR=0.8 # this value controld delay decrease for each level up
 
 ticker() {
-  while true; do echo -n $DOWN; sleep $DELAY; done
+  while true; do 
+    echo -n $DOWN;
+    sleep $DELAY;
+  done
 }
 
 reader() {
+  # "-u" means When the variable is assigned a value,
+  # all lower-case characters are converted to upper-case.
+  local -u key command
+  # "-a" means each name is an indexed array variable.
+  # "-A" means each name is an associative array variable.
+  declare -A commands=([A]=$ROTATE [C]=$RIGHT [D]=$LEFT)
+  # "-s" means read in silent mode.
+  # "-n" num means read only num's characters of input.
   while read -s -n 1 key; do
     echo -n $key
   done
@@ -28,4 +39,5 @@ controller() {
   done
 }
 
+# "&" means ticker runs as separate process
 (ticker & reader) | controller
