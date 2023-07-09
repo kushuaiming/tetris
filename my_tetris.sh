d58f97b59bbfafba3d7746f4000e724aa4a43437
kushuaiming <xm2534911078@outlook.com>
2023-07-09 21:55:27 +0800

    feat: init

--- my_tetris.sh
+++ my_tetris.sh
@@ -10,6 +10,17 @@ DROP=5
 DELAY=1          # initial delay between piece movements
 DELAY_FACTOR=0.8 # this value controld delay decrease for each level up
 
+hide_cursor() {
+  # "echo -n" means do not output the trailing newline.
+  # "echo -e" means enable interpretation of backslash escapes
+  echo -ne "\033[?25l"
+}
+
+init() {
+  clear
+  hide_cursor
+}
+
 ticker() {
   while true; do 
     echo -n $DOWN;
@@ -18,20 +29,21 @@ ticker() {
 }
 
 reader() {
-  # "-u" means When the variable is assigned a value,
+  # "local -u" means when the variable is assigned a value,
   # all lower-case characters are converted to upper-case.
   local -u key command
-  # "-a" means each name is an indexed array variable.
-  # "-A" means each name is an associative array variable.
+  # "declare -a name" means each name is an indexed array variable.
+  # "declare -A name" means each name is an associative array variable.
   declare -A commands=([A]=$ROTATE [C]=$RIGHT [D]=$LEFT)
-  # "-s" means read in silent mode.
-  # "-n" num means read only num's characters of input.
+  # "read -s" means read in silent mode.
+  # "read -n" num means read only num's characters of input.
   while read -s -n 1 key; do
     echo -n $key
   done
 }
 
 controller() {
+  init
   local command
   while true; do
     read -s -n 1 command
